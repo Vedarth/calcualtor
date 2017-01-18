c52bf824cc8a3b917770cf76594ceb97ee49c510
Vedarth <vedarth.sharma@gmail.com>
2017-01-18 20:21:40 +0530

    Added in multiplcation too

--- calculator.py
+++ calculator.py
@@ -3,4 +3,6 @@
 a = raw_input("Enter your first number:")
 b = raw_input("Enter your second number:")
 c = int(a) + int(b)
+d = int(a) * int(b)
 print("The sum is %d"%c)
+print("The product is %d"%d)
